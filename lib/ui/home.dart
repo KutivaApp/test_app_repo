ff1aca86ca00d70977ea0b41b2b558530cbbc9f5
Nelson  Chadali <nchadaly@gmail.com>
2025-10-04 13:51:22 +0200

    KAW-3 - Implementacao da tela animada (#3)

--- lib/ui/home.dart
+++ lib/ui/home.dart
@@ -1,15 +1,65 @@
 import 'package:flutter/material.dart';
+import 'package:lottie/lottie.dart';
 
-class HomePge extends StatefulWidget {
-  const HomePge({super.key});
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
 
   @override
-  State<HomePge> createState() => _HomePgeState();
+  State<HomePage> createState() => _HomePageState();
 }
 
-class _HomePgeState extends State<HomePge> {
+class _HomePageState extends State<HomePage> {
+  int increment = 0;
+
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(backgroundColor: Colors.amber);
+    return Scaffold(
+      appBar: AppBar(title: Text("Title"), elevation: 3),
+      body: Container(
+        padding: EdgeInsets.all(16),
+        width: double.infinity,
+        child: SingleChildScrollView(
+          child: Column(
+            spacing: 20,
+            mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Lottie.asset('assets/Exams.json'),
+              const Text("Incremento"),
+              const SizedBox(height: 10),
+              Container(
+                decoration: BoxDecoration(
+                  color: Colors.greenAccent,
+                  borderRadius: BorderRadius.circular(12),
+                ),
+                child: Row(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    IconButton(
+                      onPressed: () {
+                        setState(() {
+                          if (increment > 0) increment--;
+                        });
+                      },
+                      icon: Icon(Icons.arrow_back_ios),
+                    ),
+                    Text("$increment", style: TextStyle(fontSize: 18)),
+                    IconButton(
+                      onPressed: () {
+                        setState(() {
+                          if (increment < 10) increment++;
+                        });
+                      },
+                      icon: Icon(Icons.arrow_forward_ios_outlined),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
